b2af012de36c52b5bf59a08721fe6294acfa0aa7
Matan Sandhaus <matan.sandhaus@icloud.com>
2017-07-06 08:04:08 -0700

    Initial Commit

---
+++ Practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Practice
+//
+//  Created by Matan Sandhaus on 7/6/17.
+//  Copyright © 2017 Matan Sandhaus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
